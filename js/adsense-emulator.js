a47729e19efd2588259740bc113d1033c0311085
Christian Schaefer <caefer@ical.ly>
2011-10-31 10:00:28 +0100

    dummy ad

--- js/adsense-emulator.js
+++ js/adsense-emulator.js
@@ -1,3 +1,3 @@
 
-document.write('<h3>testing adsense emulator</h3>');
+document.write('<iframe id="emulator_frame1" width="728" scrolling="no" height="90" frameborder="0" vspace="0" style="left:0;position:absolute;top:0" src="resources/emulated-superbanner.html" name="emulator_frame1" marginwidth="0" marginheight="0" hspace="0" allowtransparency="true"></iframe>');
 
