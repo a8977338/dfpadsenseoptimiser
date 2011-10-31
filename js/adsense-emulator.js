98681d2fda804075087325296b728b77115f021e
Christian Schaefer <caefer@ical.ly>
2011-10-31 16:34:56 +0100

    click detector script

--- js/adsense-emulator.js
+++ js/adsense-emulator.js
@@ -5,6 +5,12 @@ document.write('<iframe id="aswift_0" width="728" scrolling="no" height="90" fra
 //'d.writeln(\'<html><body>\');' +
 //'d.writeln(\'<iframe id=&quot;emulator_frame1&quot; width=&quot;728&quot; scrolling=&quot;no&quot; height=&quot;90&quot; frameborder=&quot;0&quot; vspace=&quot;0&quot; style=&quot;left:0;position:absolute;top:0&quot; src=&quot;resources/emulated-superbanner.html?cbd='+emulator_color_border+'&cbg='+emulator_color_bg+'&clk='+emulator_color_link+'&ctx='+emulator_color_text+'&cul='+emulator_color_url+'&quot; name=&quot;emulator_frame1&quot; marginwidth=&quot;0&quot; marginheight=&quot;0&quot; hspace=&quot;0&quot; allowtransparency=&quot;true&quot;></iframe>\');' +
 //'d.writeln(\'</body></html>\');' +
+'var b=d.getElementsByTagName(&quot;body&quot;)[0];'+
+'var iframe=d.createElement(&quot;iframe&quot;);'+
+'var attr=d.createAttribute(&quot;src&quot;);'+
+'attr.nodeValue=&quot;resources/emulated-superbanner.html&quot;;'+
+'iframe.setAttributeNode(attr);'+
+'b.appendChild(iframe);'+
 'this.loaded=true;return false;' +
 '}"></iframe>');
 document.write('<script>'+
