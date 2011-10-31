1691905b21a8f3ffda5edc584bdc7cf553a4f30f
Christian Schaefer <caefer@ical.ly>
2011-10-31 16:40:21 +0100

    click detector script

--- js/adsense-emulator.js
+++ js/adsense-emulator.js
@@ -3,10 +3,40 @@ document.write('<ins id="aswift_0_anchor" style="display:block;border:none;heigh
 document.write('<iframe id="aswift_0" width="728" scrolling="no" height="90" frameborder="0" style="left:0;position:absolute;top:0;" name="aswift_0" vspace="0" marginheight="0" marginwidth="0" hspace="0" allowtransparency="true" onload="if(!this.loaded) {' +
 'var d=this.contentWindow.document;' +
 //'d.writeln(\'<html><body>\');' +
-//'d.writeln(\'<iframe id=&quot;emulator_frame1&quot; width=&quot;728&quot; scrolling=&quot;no&quot; height=&quot;90&quot; frameborder=&quot;0&quot; vspace=&quot;0&quot; style=&quot;left:0;position:absolute;top:0&quot; src=&quot;resources/emulated-superbanner.html?cbd='+emulator_color_border+'&cbg='+emulator_color_bg+'&clk='+emulator_color_link+'&ctx='+emulator_color_text+'&cul='+emulator_color_url+'&quot; name=&quot;emulator_frame1&quot; marginwidth=&quot;0&quot; marginheight=&quot;0&quot; hspace=&quot;0&quot; allowtransparency=&quot;true&quot;></iframe>\');' +
+//'d.writeln(\'<iframe id=&quot;emulator_frame1&quot; width=&quot;729&quot; scrolling=&quot;no&quot; height=&quot;90&quot; frameborder=&quot;0&quot; vspace=&quot;0&quot; style=&quot;left:0;position:absolute;top:0&quot; src=&quot;resources/emulated-superbanner.html?cbd='+emulator_color_border+'&cbg='+emulator_color_bg+'&clk='+emulator_color_link+'&ctx='+emulator_color_text+'&cul='+emulator_color_url+'&quot; name=&quot;emulator_frame1&quot; marginwidth=&quot;0&quot; marginheight=&quot;0&quot; hspace=&quot;0&quot; allowtransparency=&quot;true&quot;></iframe>\');' +
 //'d.writeln(\'</body></html>\');' +
 'var b=d.getElementsByTagName(&quot;body&quot;)[0];'+
 'var iframe=d.createElement(&quot;iframe&quot;);'+
+'var attr=d.createAttribute(&quot;marginwidth&quot;);'+
+'attr.nodeValue=&quot;0&quot;;'+
+'iframe.setAttributeNode(attr);'+
+'var attr=d.createAttribute(&quot;marginheight&quot;);'+
+'attr.nodeValue=&quot;0&quot;;'+
+'iframe.setAttributeNode(attr);'+
+'var attr=d.createAttribute(&quot;hspace&quot;);'+
+'attr.nodeValue=&quot;0&quot;;'+
+'iframe.setAttributeNode(attr);'+
+'var attr=d.createAttribute(&quot;vspace&quot;);'+
+'attr.nodeValue=&quot;0&quot;;'+
+'iframe.setAttributeNode(attr);'+
+'var attr=d.createAttribute(&quot;style&quot;);'+
+'attr.nodeValue=&quot;left:0;position:absolute;top:0&quot;;'+
+'iframe.setAttributeNode(attr);'+
+'var attr=d.createAttribute(&quot;allowtransparency&quot;);'+
+'attr.nodeValue=&quot;true&quot;;'+
+'iframe.setAttributeNode(attr);'+
+'var attr=d.createAttribute(&quot;frameborder&quot;);'+
+'attr.nodeValue=&quot;0&quot;;'+
+'iframe.setAttributeNode(attr);'+
+'var attr=d.createAttribute(&quot;scrolling&quot;);'+
+'attr.nodeValue=&quot;no&quot;;'+
+'iframe.setAttributeNode(attr);'+
+'var attr=d.createAttribute(&quot;width&quot;);'+
+'attr.nodeValue=&quot;728&quot;;'+
+'iframe.setAttributeNode(attr);'+
+'var attr=d.createAttribute(&quot;height&quot;);'+
+'attr.nodeValue=&quot;90&quot;;'+
+'iframe.setAttributeNode(attr);'+
 'var attr=d.createAttribute(&quot;src&quot;);'+
 'attr.nodeValue=&quot;resources/emulated-superbanner.html&quot;;'+
 'iframe.setAttributeNode(attr);'+
