5581d811abd9beb88dcf033106ce37fcde950541
Christian Schaefer <caefer@ical.ly>
2011-10-31 10:12:54 +0100

    dummy ad

--- js/adsense-emulator.js
+++ js/adsense-emulator.js
@@ -1,6 +1,6 @@
 document.write('<ins style="display:inline-table;border:none;height:90px;margin:0;padding:0;position:relative;visibility:visible;width:728px">');
 document.write('<ins id="aswift_0_anchor" style="display:block;border:none;height:90px;margin:0;padding:0;position:relative;visibility:visible;width:728px">');
-document.write('<iframe id="aswift_0" width="728" scrolling="no" height="90" frameborder="0" style="left:0;position:absolute;top:0;" name="aswift_0" vspace="0" marginheight="0" marginwidth="0" hspace="0" allowtransparency="true">');
+document.write('<iframe id="aswift_0" width="728" scrolling="no" height="90" frameborder="0" style="left:0;position:absolute;top:0;" name="aswift_0" vspace="0" marginheight="0" marginwidth="0" hspace="0" allowtransparency="true" onload="this.contentWindow.location.replace(\'resources/emulated-superbanner.html\')">');
 document.write('<html id="inner"><body>');
 document.write('<iframe id="emulator_frame1" width="728" scrolling="no" height="90" frameborder="0" vspace="0" style="left:0;position:absolute;top:0" src="resources/emulated-superbanner.html" name="emulator_frame1" marginwidth="0" marginheight="0" hspace="0" allowtransparency="true"></iframe>');
 document.write('</body></html>');
