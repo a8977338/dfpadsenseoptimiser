2265ea7c559e68910945530ed2ea890a68c1bf07
Christian Schaefer <caefer@ical.ly>
2011-10-31 16:18:16 +0100

    click detector script

--- js/adsense-emulator.js
+++ js/adsense-emulator.js
@@ -2,9 +2,10 @@ document.write('<ins style="display:inline-table;border:none;height:90px;margin:
 document.write('<ins id="aswift_0_anchor" style="display:block;border:none;height:90px;margin:0;padding:0;position:relative;visibility:visible;width:728px">');
 document.write('<iframe id="aswift_0" width="728" scrolling="no" height="90" frameborder="0" style="left:0;position:absolute;top:0;" name="aswift_0" vspace="0" marginheight="0" marginwidth="0" hspace="0" allowtransparency="true" onload="if(!this.loaded) {' +
 'var d=this.contentWindow.document;' +
-'d.writeln(\'<html><body>\');' +
+//'d.writeln(\'<html><body>\');' +
 //'d.writeln(\'<iframe id=&quot;emulator_frame1&quot; width=&quot;728&quot; scrolling=&quot;no&quot; height=&quot;90&quot; frameborder=&quot;0&quot; vspace=&quot;0&quot; style=&quot;left:0;position:absolute;top:0&quot; src=&quot;resources/emulated-superbanner.html?cbd='+emulator_color_border+'&cbg='+emulator_color_bg+'&clk='+emulator_color_link+'&ctx='+emulator_color_text+'&cul='+emulator_color_url+'&quot; name=&quot;emulator_frame1&quot; marginwidth=&quot;0&quot; marginheight=&quot;0&quot; hspace=&quot;0&quot; allowtransparency=&quot;true&quot;></iframe>\');' +
-'d.writeln(\'</body></html>\');' +
+//'d.writeln(\'</body></html>\');' +
 'this.loaded=true;return false;' +
 '}"></iframe>');
+document.write('<script>alert(this);</script>');
 document.write('</ins></ins>');
